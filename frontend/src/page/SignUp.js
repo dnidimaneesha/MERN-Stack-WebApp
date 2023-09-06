dbec4a8abf957ac953cdacb38ee5891ecfffe5ea
dnidimaneesha <96563@fhss.sjp.ac.lk>
2023-09-06 15:36:57 +0530

    complete signUp page UI

--- frontend/src/page/SignUp.js
+++ frontend/src/page/SignUp.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import loginSignupImage from "../assest/loginSignupImage.gif";
 import {BiShow, BiHide} from 'react-icons/bi'
 import "../component/SignUp.css";
+import { Link } from "react-router-dom";
 
 
 
@@ -19,17 +20,17 @@ const SignUp = () => {
   return (
   
     <div className="signup__image--container">
+
     <div className='p-3 md:p-4'>
-      <div className='w-full max-w-sm bg-white m-auto flex justify-center items-center flex-col p-4'>
-        {/*<h1 className='text-center text-2xl font-bold'>sign up</h1>*/}
-        <div className='w-20 overflow-hidden rounded-full drop-shadow-md shadow-md'>
+    <div className="w-full max-w-sm bg-white m-auto flex  flex-col p-4">
+        
+    <div className="w-20 h-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto relative ">
         <img src={loginSignupImage} className="w-full"/>
         </div>
 
-        <form className='w-full py-3'>
-
+        <form className="w-full py-3 flex flex-col" >
           <label htmlFor='firstName' >Frist Name</label>
-          <input type={"text"} id="firstName" name='fistName' className='mt-1 mb-2 w-full bg-slate-200 p-1 px-2 py-1 rounded focus-within:outline-blue-300'/>
+          <input type={"text"} id="firstName" name='fistName' className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"/>
 
           <label htmlFor='lastName'>Last Name</label>
           <input type={"text"} id="lirstName" name='listName' className='mt-1 mb-2 w-full bg-slate-200 p-1 px-2 py-1 rounded focus-within:outline-blue-300'/>
@@ -43,7 +44,6 @@ const SignUp = () => {
           <span className="flex text-xl cursor-pointer" onClick={handleShowPassword}>{showPassword ? <BiShow /> : <BiHide />}
           </span>
           </div>
-
           
           <label htmlFor='confirmpassword'>Confirm Password</label>
           <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300">
@@ -51,8 +51,16 @@ const SignUp = () => {
           <span className="flex text-xl cursor-pointer" onClick={handleShowConfirmPassword}>{showConfirmPassword ? <BiShow /> : <BiHide />}
           </span>
           </div>
-          <button className="max-w[120px] w-full bg-emerald-600 hover:bg-emerald-700">Sign up</button>
+
+          <button className="w-full max-w-[150px] m-auto  bg-green-400 hover:bg-green-600 cursor-pointer  text-white text-xl font-medium text-center py-1 rounded-full mt-4 ">Sign up</button>
         </form>
+   
+        <p className="text-left text-sm mt-1 pb-5">
+          Already have account ?{" "}
+          <Link to={"/login"} className="text-red-500 underline ">
+            Login
+          </Link>
+          </p>
       </div>
       </div>
       </div>
